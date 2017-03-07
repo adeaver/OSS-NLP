f801098a698b2baa04234ad207ee03e800c6bc63
adeaver <deaver.andrew@gmail.com>
2017-03-07 18:52:27 +0100

    Added data population for Earley parser

--- POSTagging/src/test/java/com/ardeaver/postagging/markov/MarkovTest.java
+++ POSTagging/src/test/java/com/ardeaver/postagging/markov/MarkovTest.java
@@ -28,4 +28,28 @@ public class MarkovTest {
 		
 		assertEquals(8, tagged.split("\\s+").length);
 	}
+	
+	@Test
+	public void testProperNouns() {
+		String tagged = markov.tagSentence("President Obama is the current president.");
+		System.out.println(tagged);
+		
+		assertEquals(6, tagged.split("\\s+").length);
+	}
+	
+	@Test
+	public void testIncorrect() {
+		String tagged = markov.tagSentence("She has patient when she teach me difficult word.");
+		System.out.println(tagged);
+		
+		assertEquals(9, tagged.split("\\s+").length);
+	}
+	
+	@Test
+	public void testPhrasalVerbs() {
+		String tagged = markov.tagSentence("But won't you take care of me when I'm old");
+		System.out.println(tagged);
+		
+		assertEquals(10, tagged.split("\\s+").length);
+	}
 }
